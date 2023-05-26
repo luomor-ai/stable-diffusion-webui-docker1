821fc56a539bf61633ddbf0787d18c2bb518b023
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-05-26 18:37:15 +0800

    update

--- services/AUTOMATIC1111/clone.sh
+++ services/AUTOMATIC1111/clone.sh
@@ -6,6 +6,6 @@ mkdir -p /repositories/"$1"
 cd /repositories/"$1"
 git init
 git remote add origin "$2"
-git fetch origin "$3" --depth=1
-git reset --hard "$3"
+cur=`git fetch origin "$3" --depth=1`
+cur=`git reset --hard "$3"`
 rm -rf .git
